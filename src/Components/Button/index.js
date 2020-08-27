c2935647e9af74f885d31efd7019f036aae03990
Vitor Gabriel do Couto <59032828+VituBR19@users.noreply.github.com>
2020-08-27 17:05:36 -0300

    Substitui TouchableOpacity por RectButton

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { View, Text, ImageBackground, TextInput, TouchableOpacity } from 'react-native'
 
 import styles from './style'
+import { RectButton } from 'react-native-gesture-handler'
 
 export default function Inputs({ title }) {
   return(
    <>
-    <TouchableOpacity style={styles.button} value='Cadastrar'>
+    <RectButton style={styles.button} value='Cadastrar'>
       <Text style={styles.buttonTitle}>{ title }</Text>
-    </TouchableOpacity>
+    </RectButton>
    </>
   )
 }
