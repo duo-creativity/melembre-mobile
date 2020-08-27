4cc27051d3979fd510dee111f5ee56285f120b73
Vitor Gabriel do Couto <59032828+VituBR19@users.noreply.github.com>
2020-08-27 17:05:54 -0300

    Adiciona placeholder

--- src/Components/Inputs/index.js
+++ src/Components/Inputs/index.js
@@ -4,14 +4,15 @@ import { View, Text, ImageBackground, TextInput } from 'react-native'
 import styles from './style'
 
 export default function Inputs({ placeholder }) {
-  const [content, setContent] = useState(placeholder)  
+  const [content, setContent] = useState('')  
   
   return(
    <>
     <TextInput 
+      placeholder={ placeholder }
       style={styles.input}
       value={content}
-      onChange={ e => setContent(e.target.value) }  
+      onChange={ e => setContent(e.target.value) }
     />
    </>
   )
